8a10d5f37fc7535b71ac6185fffede7b6d095727
Alysia Wong <39535350+AlysiaWong@users.noreply.github.com>
2021-10-11 18:35:45 -0700

    Testing csv file for database import
    Currently, the csv file is unreadable. Need to debug.

--- sustainableswaps/src/main/java/com/lightningducks/sustainableswaps/MySQLConnectionTest.java
+++ sustainableswaps/src/main/java/com/lightningducks/sustainableswaps/MySQLConnectionTest.java
@@ -1,11 +1,15 @@
 package com.lightningducks.sustainableswaps;
 
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import java.io.*;
+import java.util.List;
 
 @SpringBootApplication
 public class MySQLConnectionTest implements CommandLineRunner {
@@ -19,7 +23,7 @@ public class MySQLConnectionTest implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-
+        readCsv();
         String sql = "INSERT INTO test (testId, testInfo) VALUES (?, ?)";
 
         // if the table 'test' already exists, insert a new row
@@ -35,6 +39,7 @@ public class MySQLConnectionTest implements CommandLineRunner {
             jdbcTemplate.execute(createTable);
 
             insertRow(sql);
+
         }
     }
 
@@ -43,6 +48,31 @@ public class MySQLConnectionTest implements CommandLineRunner {
 
         if (result > 0) {
             System.out.println(result + "\nA new row has been inserted");
+        }
+    }
+    public void readCsv() {
+        try {
+            File file = new File("src/main/java/Products.csv");
+            System.out.println("Readable: " + file.canRead());
+
+            /* ----- REQUIRES EDITING, CURRENTLY THROWING ERRORS -----
+            //File file = new File(getClass().getResource("Products.csv").toURI());
+            FileReader filereader = new FileReader(file);
+
+            // Create csvReader object and skip first Line
+            CSVReader csvReader = new CSVReaderBuilder(filereader).withSkipLines(1).build();
+            List<String[]> allData = csvReader.readAll();
+
+            // Print results
+            for (String[] row : allData) {
+                for (String cell : row) {
+                    System.out.print(cell + "\t");
+                }
+                System.out.println();
+            }*/
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
